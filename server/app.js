7b931d833dc4d05bb2de9ccdd980afcc5bdbfb14
musabagja <bagja7musa@gmail.com>
2020-12-11 13:27:00 +0700

    Initial setup

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const { ApolloServer, gql, makeExecutableSchema } = require('apollo-server');
+
+const typeDefs = gql``;
+
+const schema = makeExecutableSchema({
+  typeDefs: [typeDefs],
+  resolvers: []
+})
+
+const server = new ApolloServer({
+  schema
+})
+
+server.listen().then(() => {
+  console.log(`
+    INSPECT Server is running!
+    Listening on port 4000
+  `);
+});
