cb1ab51b65bb8c75c4252cc5df7b8b63409cb61d
musabagja <bagja7musa@gmail.com>
2020-12-14 20:12:06 +0700

    Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,48 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { 
   HomePage, RegisterPage, LoginPage, TaxCredibilityPage, CasesOptionPage,
   Report1, Report2, Report3, ReportFinish
 } from './pages';
 import { Navbar } from './components';
+import store from './store';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
-        <Switch>
-          <Route path="/report/:report/2">
-            <Report2/>
-          </Route>
-          <Route path="/report/:report/3">
-            <Report3/>
-          </Route>
-          <Route path="/report/:report/4">
-            <ReportFinish/>
-          </Route>
-          <Route path="/report/:report">
-            <Report1/>
-          </Route>
-          <Route path='/sign-in'>
-            <LoginPage/>
-          </Route>
-          <Route path='/sign-up'>
-            <RegisterPage/>
-          </Route>
-          <Route path="/c">
-            <CasesOptionPage/>
-          </Route>
-          <Route path="/tax-and-credibility">
-            <TaxCredibilityPage/>
-          </Route>
-          <Route exact path="/">
-            <HomePage/>
-          </Route>
-        </Switch>
+        <Provider store={ store }>
+          <Navbar/>
+          <Switch>
+            <Route path="/report/:report/2">
+              <Report2/>
+            </Route>
+            <Route path="/report/:report/3">
+              <Report3/>
+            </Route>
+            <Route path="/report/:report/4">
+              <ReportFinish/>
+            </Route>
+            <Route path="/report/:report">
+              <Report1/>
+            </Route>
+            <Route path='/sign-in'>
+              <LoginPage/>
+            </Route>
+            <Route path='/sign-up'>
+              <RegisterPage/>
+            </Route>
+            <Route path="/c">
+              <CasesOptionPage/>
+            </Route>
+            <Route path="/tax-and-credibility">
+              <TaxCredibilityPage/>
+            </Route>
+            <Route exact path="/">
+              <HomePage/>
+            </Route>
+          </Switch>
+        </Provider>
       </Router>
     </div>
   );
