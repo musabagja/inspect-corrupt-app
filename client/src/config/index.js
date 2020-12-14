8ecb0b5163643b15cfb15f3cac89756d5ac65e33
yogi3915 <yogi_ferdian3915@yahoo.com>
2020-12-15 06:40:11 +0700

    report 2, 3 to finish

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,8 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache()
 });
+
+export const reportData2 = makeVar({
+  dateHappend:'',
+  description: '',
+  isDocumentProvided: '',
+  involvedPerson: '',
+  personRole: '',
+  isReported: '',
+})
+
+export const reportData3 = makeVar({
+  isKeepInTouch: '',
+  aboutInspectApp: ''
+})
 
 export default client
